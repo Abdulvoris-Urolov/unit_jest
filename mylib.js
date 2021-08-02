e8173751d9f777b66804522034ab78f9a2e2fc63
Abdulvorisjs <abdulvoris000urolov@gmail.com>
2021-08-02 12:27:09 +0500

    change

--- mylib.js
+++ mylib.js
@@ -1,1 +1,4 @@
 //Sonlarni test qilish - mutlaq qiymati
+module.exports = function (number) {
+    return (number >= 0) ? number : -number;
+}
--- test/mylib.test.js
+++ test/mylib.test.js
@@ -1,3 +1,18 @@
-test('Mening ilk testim', () =>{
-    throw new Error('Nimadir iwlamay qoldi');
-}); 
+const myLib = require('../mylib');
+
+describe('abs', () =>{
+    it('should return a positive number if input is positive', () =>{
+        const result = myLib(1);
+        expect(result).toBe(1); 
+    }); 
+    
+    it('should return a positive number if input is negative', () =>{
+        const result = myLib(-1);
+        expect(result).toBe(1); 
+    }); 
+    
+    it('should return a 0 number if input is 0', () =>{
+        const result = myLib(0);
+        expect(result).toBe(0); 
+    }); 
+});
